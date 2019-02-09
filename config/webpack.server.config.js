2dd10ddb32585006abd5dd6106b069e5a992d627
young <yangpeifang@macdeMacBook-Pro-2.local>
2019-02-09 16:31:53 +0800

    zancun

--- config/webpack.server.config.js
+++ config/webpack.server.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 // const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
 // const ignoredFiles = require('react-dev-utils/ignoredFiles');
 const config = require('./webpack.dev.config');
-// const paths = require('./paths');
+const paths = require('./paths');
 
 // const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || '0.0.0.0';
@@ -32,7 +32,7 @@ module.exports = function(proxy, allowedHost) {
     // for files like `favicon.ico`, `manifest.json`, and libraries that are
     // for some reason broken when imported through Webpack. If you just want to
     // use an image, put it in `src` and `import` it from JavaScript instead.
-    contentBase: path.join(__dirname, '../public'),
+    contentBase: paths.appPublic,
     // By default files from `contentBase` will not trigger a page reload.
     watchContentBase: true,
     // Enable hot reloading server. It will provide /sockjs-node/ endpoint
